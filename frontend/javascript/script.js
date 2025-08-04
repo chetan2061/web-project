474f16a3007b0558389948a8a3d6500c09e07570
Chetan Singh <chetansingh206111@gmail.com>
2025-08-04 16:11:54 +0545

    html

--- frontend/javascript/script.js
+++
@@ -1,7 +0,0 @@
-function getMessage() {
-  fetch("/api/message")
-    .then((res) => res.json())
-    .then((data) => {
-      document.getElementById("output").textContent = data.message;
-    });
-}
